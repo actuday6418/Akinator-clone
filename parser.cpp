f2bc39c32ba6f7de7dba4fbe23b787996d3b6572
actuday6418 <actuday6418@gmail.com>
2020-02-24 08:30:03 +0530

    All edits have been done with a PHONE!9

---
+++ parser.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<fstream>
+
+int main()
+{
+	std::fstream file("mn.cpp",std::ios::in | std::ios::out);
+	std::string line;
+	bool flag = false;
+	if(file.is_open())
+	{
+	while(getline(file,line))
+	{
+	if(!line.compare("//PARSER-FLAG"))
+	{
+		flag = true;
+		file.seekp(file.tellg());
+		file.seekp(-14,std::ios::cur);
+	        file<<"helloworld\n//PARSER-FLAG\n}";
+		system("g++ mn.cpp");
+		break;
+	}
+	}
+	if(!flag)
+	std::cout<<std::endl<<"E: PARSER FLAG not found";
+	}
+	else std::cout<<std::endl<<"E: Trouble opening the file";
+std::cout<<std::endl;
+}
